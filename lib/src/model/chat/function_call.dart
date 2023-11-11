c1365b6e56fc082cbba58f42abfcf8a51635321c
wilinz <weilizan71@gmail.com>
2023-11-12 03:02:21 +0800

    update function_call.dart

--- lib/src/model/chat/function_call.dart
+++ lib/src/model/chat/function_call.dart
@@ -1,9 +1,14 @@
+import 'package:autoequal/autoequal.dart';
+import 'package:copy_with_extension/copy_with_extension.dart';
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'function_call.g.dart';
 
+@CopyWith()
+@autoequal
 @JsonSerializable(explicitToJson: true)
-class FunctionCall {
+class FunctionCall with EquatableMixin {
   @JsonKey(name: 'name', includeIfNull: false)
   final String name;
 
@@ -19,4 +24,7 @@ class FunctionCall {
       _$FunctionCallFromJson(json);
 
   Map<String, dynamic> toJson() => _$FunctionCallToJson(this);
+
+  @override
+  List<Object?> get props => _$props;
 }
