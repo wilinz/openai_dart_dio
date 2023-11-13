188bf1f23ea9303b60cbcaa2dcbebb7035bc6c41
wilinz <weilizan71@gmail.com>
2023-11-14 01:21:01 +0800

    update

--- lib/openai_dart_dio.dart
+++ lib/openai_dart_dio.dart
@@ -1,7 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/api/openai_client.dart';
