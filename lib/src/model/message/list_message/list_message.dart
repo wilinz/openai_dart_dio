cf0fba80ab20d8a856f57c657582288053396883
wilinz <weilizan71@gmail.com>
2023-11-14 02:05:57 +0800

    update

--- lib/src/model/message/list_message/list_message.dart
+++ lib/src/model/message/list_message/list_message.dart
@@ -33,40 +33,5 @@ class ListMessage {
   Map<String, dynamic> toJson() => _$ListMessageToJson(this);
 }
 
-@JsonSerializable(explicitToJson: true)
-class Text {
-  Text(
-      {required this.value,
-      required this.annotations});
-
-  @JsonKey(name: "value", defaultValue: "")
-  String value;
-
-  @JsonKey(name: "annotations")
-  List annotations;
-
-
-  factory Text.fromJson(Map<String, dynamic> json) => _$TextFromJson(json);
-
-  Map<String, dynamic> toJson() => _$TextToJson(this);
-}
-
-@JsonSerializable(explicitToJson: true)
-class Content {
-  Content(
-      {required this.type,
-      required this.text});
-
-  @JsonKey(name: "type", defaultValue: "")
-  String type;
-
-  @JsonKey(name: "text")
-  Text text;
-
-
-  factory Content.fromJson(Map<String, dynamic> json) => _$ContentFromJson(json);
-
-  Map<String, dynamic> toJson() => _$ContentToJson(this);
-}
 
 
